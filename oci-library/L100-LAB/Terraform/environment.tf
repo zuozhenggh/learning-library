22226453e286214a2d1dfd14e97cddc4bb7ab217
jdavies <jeff.davies@yahoo.com>
2020-01-29 10:56:16 -0600

    removed compartment_id from the provider definition

--- oci-library/L100-LAB/Terraform/environment.tf
+++ oci-library/L100-LAB/Terraform/environment.tf
@@ -43,7 +43,6 @@ variable "compartment_ocid" {
 provider "oci" {
   version          = ">= 3.0.0"
   tenancy_ocid     = var.tenancy_ocid
-  compartment_ocid = var.compartment_ocid
   user_ocid        = var.user_ocid
   fingerprint      = var.fingerprint
   private_key_path = var.private_key_path
