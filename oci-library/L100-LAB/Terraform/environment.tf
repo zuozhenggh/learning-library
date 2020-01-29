b47936632c39f41ce190c34019a883a41400d5ef
jdavies <jeff.davies@yahoo.com>
2020-01-29 10:50:06 -0600

    fixed duplicae variable def for compartment_ocid

--- oci-library/L100-LAB/Terraform/environment.tf
+++ oci-library/L100-LAB/Terraform/environment.tf
@@ -8,21 +8,23 @@
 variable "tenancy_ocid" {
   type = string
 }
-variable "compartment_ocid" {
-  type = string
-}
+
 variable "user_ocid" {
   type = string
 }
+
 variable "fingerprint" {
   type = string
 }
+
 variable "private_key_path" {
   type = string
 }
+
 variable "region" {
   type = string
 }
+
 variable "region_name" {
   type = string
 }
