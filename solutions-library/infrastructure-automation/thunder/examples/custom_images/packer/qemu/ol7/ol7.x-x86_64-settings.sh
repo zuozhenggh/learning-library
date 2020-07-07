9061ae28642361064f89f765c2f48db2b8044583
Flavius Dinu <flavius.dinu@oracle.com>
2020-07-07 17:50:07 +0300

    Sanitized all examples

--- solutions-library/infrastructure-automation/thunder/examples/custom_images/packer/qemu/ol7/ol7.x-x86_64-settings.sh
+++
@@ -1,6 +0,0 @@
-echo "export PS1=\"\$USER@\$(hostname): \w \\\\$ \"" >> /root/.bashrc
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir -p /etc/dhcp/exit-hooks.d/
-# lock root account
-passwd -d root
-passwd -l root
--- solutions-library/infrastructure-automation/thunder/examples/run/glusterfs/examples/georeplicated_glusterfs_volume/distributed_replicated/outputs.tf
+++
@@ -1,9 +0,0 @@
- //Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
-
-output "private_ips_region1" {
-  value = module.glusterfs_region_1.private_ips
-}
-
-output "private_ips_region2" {
-  value = module.glusterfs_region_2.private_ips
-}
